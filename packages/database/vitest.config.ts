b45a1c6521b5a5e7665e19c202b120eddcaab43b
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-07-19 16:24:37 +0200

    github reporter

---
+++ packages/database/vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+		test: {
+			reporters: process.env.GITHUB_ACTIONS
+				? ["dot", "github-actions"]
+				: ["dot"],
+		},
+	})

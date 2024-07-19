e0d5929b40da5c99e6f02416cfb48bbff35c0e82
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-07-19 16:34:55 +0200

    debug ci

--- packages/database/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-		test: {
-			reporters: process.env.GITHUB_ACTIONS
-				? ["dot", "github-actions"]
-				: ["dot"],
-		},
-	})

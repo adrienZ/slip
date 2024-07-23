b4afdbb8ca632983d54beecbc45ae80d1c902afe
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-07-23 16:15:59 +0200

    nitro plugin add routes

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -1,14 +1,47 @@
 import { checkDbAndTables, type tableNames } from "@slip/database";
-import type { Database } from "db0";
+// import { hash } from "@node-rs/argon2";
 
 type checkDbAndTablesParameters = Parameters<typeof checkDbAndTables>;
 
-export class SlipAuth {
+export class SlipAuthCore {
+  #db: checkDbAndTablesParameters[0]
+  #tableNames: tableNames
+  // #PASSWORD_SALT = "SLIP_SRKWkJH.}!JgT0;6MgaN?R=bTCKg*:_"
+
   constructor(
     providedDatase: checkDbAndTablesParameters[0],
     dialect: checkDbAndTablesParameters[1],
     tableNames: tableNames
   ) {
     checkDbAndTables(providedDatase, dialect, tableNames);
+
+    this.#db = providedDatase;
+    this.#tableNames = tableNames;
+  }
+
+
+  // private hasPassword(password: string) {
+  //   return hash(this.PASSWORD_SALT + password);
+  // }
+
+  async registerUser(id: string, email: string) {
+    const insterted = await  this.#db.prepare(`INSERT INTO ${this.#tableNames.users} (id, email) VALUES ('${id}', '${email}')`).run()
+    const users = await this.#db.prepare(`SELECT * FROM ${this.#tableNames.users} WHERE id = '${id}'`).all();
+    return users[0];
+  }
+
+
+  async loginUser(email: string) {
+    const rows = await this.#db.prepare(`SELECT * FROM ${this.#tableNames.users} WHERE email = '${email}'`).all()
+    const user = rows.at(1);
+
+    if (!user) {
+			return createError({
+				statusCode: 401,
+				statusMessage: "Invalid username or password",
+			});
+		}
+
+    return user;
   }
 }
--- packages/nitro/server/plugins/plugin.test.ts
+++ packages/nitro/server/plugins/plugin.test.ts
@@ -1,4 +1,5 @@
-import { SlipAuth } from "@slip/core"
+import { SlipAuthCore } from "@slip/core"
+import {  createAuthRoutes } from "../../lib/nitro"
 
 export default defineNitroPlugin(async (nitroApp) => {
   const db = useDatabase();
@@ -7,28 +8,14 @@ export default defineNitroPlugin(async (nitroApp) => {
   await db.sql`CREATE TABLE IF NOT EXISTS users ("id" TEXT NOT NULL PRIMARY KEY, "email" TEXT NOT NULL)`;
   await db.sql`CREATE TABLE IF NOT EXISTS sessions ("id" TEXT NOT NULL PRIMARY KEY, "expires_at" INTEGER NOT NULL, "user_id" TEXT NOT NULL, FOREIGN KEY (user_id) REFERENCES user(id))`;
 
-  const Auth = new SlipAuth(db, "sqlite", {
+  const slip = new SlipAuthCore(db, "sqlite", {
     users: "users",
     sessions: "sessions"
-  });
-  
-  console.log({ Auth });
+  })
 
-  nitroApp.router.add("/auth/register", defineEventHandler(async event => {
-    // Add a new user
-    const userId = String(Math.round(Math.random() * 10_000));
-    await db.sql`INSERT INTO users VALUES (${userId}, 'test@test.mail')`;
+  createAuthRoutes(nitroApp.router, slip);
 
-    // Query for users
-    const { rows } = await db.sql`SELECT * FROM users WHERE id = ${userId}`;
-
-    return {
-      rows,
-    };
-  }), "get")
-
-
-  nitroApp.router.add("/auth/users", defineEventHandler(async event => {
+  nitroApp.router.add("/users", defineEventHandler(async event => {
     // Query for users
     const { rows } = await db.sql`SELECT * FROM users`;
 

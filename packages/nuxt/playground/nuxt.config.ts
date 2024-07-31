22b0d95461e42c6dd6014cdad94af081b8819c9c
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-07-31 15:14:56 +0200

    fix lint

--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['nuxt-auth-utils', "../src/module"],
+  modules: ["nuxt-auth-utils", "../src/module"],
   devtools: { enabled: true },
   compatibilityDate: "2024-07-27",
 });

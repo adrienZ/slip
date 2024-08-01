1db20020846524f79ca550e080410e8a3638dc2f
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-08-01 17:52:07 +0200

    format

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -4,5 +4,5 @@ export default defineBuildConfig({
   entries: ["./src/core"],
   declaration: true,
   // we have warnings because we use publishConfig in package.json
-  failOnWarn: false
-})
+  failOnWarn: false,
+});

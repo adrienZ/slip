601bda3d6bcc94502220fde485d6a3b9f039d280
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-08-31 03:17:43 +0200

    fix setup

--- playground/server/plugins/database.setup.ts
+++ playground/server/plugins/database.setup.ts
@@ -1,7 +1,7 @@
 export default defineNitroPlugin(async () => {
   const db = useDatabase();
 
-  await db.sql`CREATE TABLE IF NOT EXISTS slip_auth_users ("id" TEXT NOT NULL PRIMARY KEY, "email" TEXT NOT NULL)`;
+  await db.sql`CREATE TABLE IF NOT EXISTS slip_auth_users ("id" TEXT NOT NULL PRIMARY KEY, "email" TEXT NOT NULL UNIQUE)`;
   await db.sql`CREATE TABLE IF NOT EXISTS slip_auth_sessions ("id" TEXT NOT NULL PRIMARY KEY, "expires_at" INTEGER NOT NULL, "user_id" TEXT NOT NULL, FOREIGN KEY (user_id) REFERENCES slip_auth_users(id))`;
   await db.sql`CREATE TABLE IF NOT EXISTS slip_auth_oauth_accounts ("provider_id" TEXT NOT NULL, "provider_user_id" TEXT NOT NULL, "user_id" TEXT NOT NULL, PRIMARY KEY (provider_id, provider_user_id), FOREIGN KEY (user_id) REFERENCES slip_auth_users(id))`;
 

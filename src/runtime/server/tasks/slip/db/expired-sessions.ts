0e6b9358589e4acc752e2ecc0f9d0e2025f460ff
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-09-03 12:40:53 +0200

    fix types

--- src/runtime/server/tasks/slip/db/expired-sessions.ts
+++ src/runtime/server/tasks/slip/db/expired-sessions.ts
@@ -1,14 +1,15 @@
 // @ts-expect-error experimental feature
-import { defineTask } from "#imports";
+import { defineTask, type runTask } from "#imports";
 import { useSlipAuth } from "../../../utils/useSlipAuth";
+
+type FixedTaskEvent = Required<NonNullable<Parameters<typeof runTask>[1]>>
 
 export default defineTask({
   meta: {
     name: "slip:db:expired-sessions",
     description: "Delete expired items in session's table",
   },
-  async run({ payload }) {
-    // @ts-expect-error CI fails otherwise
+  async run({ payload }: FixedTaskEvent) {
     const scheduledTime = payload.scheduledTime as number;
     console.log("Running SLIP expired sessions task...", scheduledTime);
     const auth = useSlipAuth();

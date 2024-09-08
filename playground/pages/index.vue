c41711fce2d1783d476063cd3e6b95dcb595ea66
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-09-08 16:31:09 +0200

    fix typing

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 const { loggedIn, user, session, clear } = useUserSession();
 
 const { data: usersInDb } = await useAsyncData("usersInDb", () => $fetch("/api/users"));
@@ -29,10 +29,9 @@ const selected = computed({
   },
 });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function sendForm(item: any) {
-  const form = document.querySelector(`.${item.label.toLowerCase()} form`) as HTMLFormElement;
-  const submitButton = document.querySelector(`.${item.label.toLowerCase()} [formaction]`) as HTMLElement;
+function sendForm(item) {
+  const form = document.querySelector(`.${item.label.toLowerCase()} form`);
+  const submitButton = document.querySelector(`.${item.label.toLowerCase()} [formaction]`);
 
   if (form && submitButton) {
     form.action = submitButton.getAttribute("formaction") ?? "";
@@ -49,57 +48,59 @@ function loginToGithub() {
 </script>
 
 <template>
-  <div
-    v-if="loggedIn"
-    class="mt-12 prose"
-  >
-    <h1 class="text-gray-900 dark:text-white mb-0">
-      Welcome {{ user.id }}!
-    </h1>
-    <p class="text-gray-900 dark:text-white">
-      Logged in until {{ new Date(session.expires_at).toDateString() }}
-    </p>
-
-    <UDivider class="my-4" />
-
-    <UButton
-      color="red"
-      @click="clear"
+  <main>
+    <div
+      v-if="loggedIn"
+      class="mt-12 prose"
     >
-      Logout
-    </UButton>
-  </div>
-  <div v-else>
-    <UTabs
-      v-model="selected"
-      :items="tabs"
-    >
-      <template #item="{ item }">
-        <UAuthForm
-          class="mx-auto mt-12"
-          :title="item.label"
-          :class="`${item.label.toLowerCase()}`"
-          align="top"
-          :fields="[{ type: 'email', name: 'email', label: 'Email', placeholder: 'Enter your email', color: 'gray' }, { type: 'password', name: 'password', label: 'Password', placeholder: 'Enter your password', color: 'gray' }]"
-          :providers="[{ label: 'GitHub', icon: 'i-simple-icons-github', color: 'gray', click: loginToGithub }]"
-          :submit-button="{ label: 'Submit', type: 'submit', formmethod: 'post', formaction: item.label === 'Register' ? '/auth/register' : '/auth/login' }"
-          @submit="sendForm(item)"
-        />
-        <UAlert
-          :close-button="{ icon: 'i-heroicons-x-mark-20-solid', color: 'gray', variant: 'link', padded: false }"
-          color="orange"
-          class="max-w-sm mt-2 mx-auto"
-          variant="subtle"
-          title="Warning !"
-          description="Github or any OAuth provider will not work inside an iframe, Stackblitz or Codesandbox"
-        />
-      </template>
-    </UTabs>
-  </div>
+      <h1 class="text-gray-900 dark:text-white mb-0">
+        Welcome {{ user.id }}!
+      </h1>
+      <p class="text-gray-900 dark:text-white">
+        Logged in until {{ new Date(session.expires_at).toDateString() }}
+      </p>
 
-  <UTable
-    v-if="usersInDb"
-    class="mt-12"
-    :rows="usersInDb.rows"
-  />
+      <UDivider class="my-4" />
+
+      <UButton
+        color="red"
+        @click="clear"
+      >
+        Logout
+      </UButton>
+    </div>
+    <div v-else>
+      <UTabs
+        v-model="selected"
+        :items="tabs"
+      >
+        <template #item="{ item }">
+          <UAuthForm
+            class="mx-auto mt-12"
+            :title="item.label"
+            :class="`${item.label.toLowerCase()}`"
+            align="top"
+            :fields="[{ type: 'email', name: 'email', label: 'Email', placeholder: 'Enter your email', color: 'gray' }, { type: 'password', name: 'password', label: 'Password', placeholder: 'Enter your password', color: 'gray' }]"
+            :providers="[{ label: 'GitHub', icon: 'i-simple-icons-github', color: 'gray', click: loginToGithub }]"
+            :submit-button="{ label: 'Submit', type: 'submit', formmethod: 'post', formaction: item.label === 'Register' ? '/auth/register' : '/auth/login' }"
+            @submit="sendForm(item)"
+          />
+          <UAlert
+            :close-button="{ icon: 'i-heroicons-x-mark-20-solid', color: 'gray', variant: 'link', padded: false }"
+            color="orange"
+            class="max-w-sm mt-2 mx-auto"
+            variant="subtle"
+            title="Warning !"
+            description="Github or any OAuth provider will not work inside an iframe, Stackblitz or Codesandbox"
+          />
+        </template>
+      </UTabs>
+    </div>
+
+    <UTable
+      v-if="usersInDb"
+      class="mt-12"
+      :rows="usersInDb.rows"
+    />
+  </main>
 </template>

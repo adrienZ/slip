7a489f90e56096838d668e4eb19771a030926047
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-09-15 19:15:13 +0200

    feat(users): add email_verified column

--- src/runtime/database/lib/schema.ts
+++ src/runtime/database/lib/schema.ts
@@ -11,6 +11,7 @@ export const getUsersTableSchema = (tableNames: tableNames) => sqliteTable(table
   id: text("id").primaryKey().notNull(),
   password: text("password"),
   email: text("email").notNull().unique(),
+  email_verified: integer("email_verified", { mode: "boolean" }).default(false),
   ...datesColumns,
 });
 

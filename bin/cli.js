d4d636a2ea1dd54216f44e963ded7b1a96964ab7
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-09-25 15:07:48 +0200

    refactor(cli): fetch files from github instead of harcoding them in cli source code

--- bin/cli.js
+++ bin/cli.js
@@ -75,8 +75,7 @@ const main = defineCommand({
           path: envFilePath,
         });
 
-        // @ts-expect-error TODO: fix the typing
-        const parsedConfig = new Map(Object.entries(dotEnvConfig.parsed));
+        const parsedConfig = new Map(Object.entries(dotEnvConfig.parsed ?? {}));
 
         if (!parsedConfig.has("NUXT_OAUTH_GITHUB_CLIENT_ID")) {
           parsedConfig.set("NUXT_OAUTH_GITHUB_CLIENT_ID", "\"\"");
@@ -101,38 +100,9 @@ const main = defineCommand({
           logger.warn("github demo already setup");
         }
         catch {
-          await createFile(githubHandlerFilePath, `
-export default oauthGitHubEventHandler({
-  config: {
-    emailRequired: true,
-  },
-  async onSuccess(event, { user }) {
-    const auth = useSlipAuth();
-
-    const [userId, sessionFromDb] = await auth.OAuthLoginUser({
-      email: user.email,
-      providerId: "github",
-      providerUserId: user.id,
-      ua: getRequestHeader(event, "User-Agent"),
-      ip: getRequestIP(event),
-    });
-
-    await setUserSession(event, {
-      expires_at: sessionFromDb.expires_at,
-      id: sessionFromDb.id,
-      user: {
-        id: userId,
-      },
-    });
-    return sendRedirect(event, "/");
-  },
-  // Optional, will return a json error and 401 status code by default
-  onError(event, error) {
-    console.error("GitHub OAuth error:", error);
-    return sendRedirect(event, "/");
-  },
-});
-          `);
+          logger.info("fetching github handler on github");
+          const githubFileRequest = await fetch("https://raw.githubusercontent.com/adrienZ/slip/refs/heads/master/playground/server/routes/auth/github.get.ts");          
+          await createFile(githubHandlerFilePath, await githubFileRequest.text());
           logger.success("github demo route setup");
         }
 

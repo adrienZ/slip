5ad187c53aeedab06a4c9d6c112d8f34d1dc7835
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-09-25 14:04:28 +0200

    fix typecheck

--- src/runtime/core/core.ts
+++ src/runtime/core/core.ts
@@ -66,7 +66,7 @@ export class SlipAuthCore {
 
     this.#passwordHashingMethods = {
       hash: defaultHashPasswordMethod,
-      verify: defaultHashPasswordMethod,
+      verify: defaultVerifyPasswordMethod,
     };
   }
 

e9490b7b1e973e30f2c834fff3a797c70256aec6
Adrien Zaganelli <adrienzaganelli@gmail.com>
2025-10-26 12:44:34 +0100

    oslo

--- src/runtime/core/email-and-password-utils.ts
+++ src/runtime/core/email-and-password-utils.ts
@@ -1,30 +1,36 @@
-import { generateRandomString, alphabet, sha256 } from "oslo/crypto";
+import { generateRandomString, type RandomReader } from "@oslojs/crypto/random";
 import type { Options as ArgonOptions } from "@node-rs/argon2";
 import { hash, verify } from "@node-rs/argon2";
-import { encodeHex } from "oslo/encoding";
+import { encodeHexLowerCase } from "@oslojs/encoding";
 
 /**
-  https://thecopenhagenbook.com/email-verification#input-validation
-
-  Input validation
-  Emails are complex and cannot be fully validated using Regex. Attempting to use Regex may also introduce ReDoS vulnerabilities. Do not over-complicate it:
-
-  Includes at least 1 @ character.
-  Has at least 1 character before the@.
-  The domain part includes at least 1 . and has at least 1 character before it.
-  It does not start or end with a whitespace.
-  Maximum of 255 characters.
+ * https://thecopenhagenbook.com/email-verification#input-validation
+ *
+ * Input validation
+ * Emails are complex and cannot be fully validated using Regex. Attempting to use Regex may also introduce ReDoS vulnerabilities. Do not over-complicate it:
+ *
+ * Includes at least 1 @ character.
+ * Has at least 1 character before the @.
+ * The domain part includes at least 1 . and has at least 1 character before it.
+ * It does not start or end with a whitespace.
+ * Maximum of 255 characters.
  */
 
 export function isValidEmail(email: string): boolean {
   return /.+@.+/.test(email);
 }
 
-export const defaultIdGenerationMethod = () => generateRandomString(15, alphabet("a-z", "A-Z", "0-9"));
+const random: RandomReader = {
+  read(bytes) {
+    crypto.getRandomValues(bytes);
+  },
+};
 
-export const defaultEmailVerificationCodeGenerationMethod = () => generateRandomString(6, alphabet("0-9", "A-Z"));
-export const defaultResetPasswordTokenIdMethod = () => generateRandomString(40, alphabet("0-9", "A-Z"));
-export const defaultResetPasswordTokenHashMethod = async (tokenId: string) => encodeHex(await sha256(new TextEncoder().encode(tokenId)));
+export const defaultIdGenerationMethod = () => generateRandomString(random, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 15);
+
+export const defaultEmailVerificationCodeGenerationMethod = () => generateRandomString(random, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 6);
+export const defaultResetPasswordTokenIdMethod = () => generateRandomString(random, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 40);
+export const defaultResetPasswordTokenHashMethod = async (tokenId: string) => encodeHexLowerCase((new TextEncoder().encode(tokenId)));
 
 const hashOptions: ArgonOptions = {
   // recommended minimum parameters
